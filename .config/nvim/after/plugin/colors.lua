15a4066717c2abe638d0821e740a4daf9ef79421
William Bushie <willbushie@gmail.com>
2025-06-17 16:30:13 -0500

    switch to default colosheme when using Apple_Terminal

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,7 +1,16 @@
 -- ensure there is always a color scheme to fall back on
 function ColorMyPencils(color) 
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
+    if vim.env.TERM_PROGRAM ~= "Apple_Terminal" then
+        target = color or "rose-pine"
+    else
+        -- target = "gruvbox"
+        target = "default"
+    end
+
+    local ok, err = pcall(vim.cmd.colorscheme, target)
+    if not ok then
+        vim.notify("Failed to load colorsheme: " .. target .. "\n" .. err, vim.log.levels.WARN)
+    end
 
     -- transparent terminal background
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
